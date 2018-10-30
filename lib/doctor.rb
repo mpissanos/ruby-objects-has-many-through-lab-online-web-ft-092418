585cfed2e3f7f7d998eebe857e487412690909ea
Manny Pissanos <mpissano@gmail.com>
2018-10-30 15:43:58 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -20,4 +20,9 @@ class Doctor
   def appointments
     Appointment.all.select { |appointment| appointment.doctor == self }
   end
+  
+  def patients
+    appointments.map(&:patient)
+  end
+  
 end
