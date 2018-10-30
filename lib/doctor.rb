4ec1fe6a24f465bbf35ace60370daf7e20929623
Manny Pissanos <mpissano@gmail.com>
2018-10-30 15:31:09 +0000

    automatically backed up by learn

---
+++ lib/doctor.rb
@@ -0,0 +1,22 @@
+class Doctor 
+
+attr_accessor :name
+
+@@all = []
+
+def initialize(name)
+  @name = name
+  @@all << self
+end
+
+def self.all
+  @@all
+end
+
+def new_appointment
+  
+end
+
+def appointments
+  
+end
